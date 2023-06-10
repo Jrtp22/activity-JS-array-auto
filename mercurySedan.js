71d9e19834fad96cbd59a745826f8c10c4956526
jrtp22 <jrtp22@gmail.com>
2023-06-10 10:11:02 -0700

    finished

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,27 +1,29 @@
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-
 //After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
+class car extends Vehicle {
+    constructor(maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel, scheduleService, make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    //loadPassenger was not givin any logic in the activity
+    loadPassenger(num)
+    //didnt add start cause super will call it no need to have it twise
+    scheduleService(mileage) {
+        if (mileage > 30000) { this.scheduleService = true return this.scheduleService }
+    }
+}
 
 
 //TO DO: Creating Instances and Testing Them
